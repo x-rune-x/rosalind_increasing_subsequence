f9b9335d6885aab60601295e23d7c28b349b45cd
James Bedford <jamesbedford@protonmail.com>
2021-04-30 18:38:13 +1000

    Attempt at recursive solution

---
+++ main.py
@@ -0,0 +1,15 @@
+def longest_subsequence(start_position, permutation):
+    subsequences = []
+    if start_position == len(permutation):
+        return subsequences
+    else:
+        for i in permutation:
+            current_subsequence = [i]
+            for j in range(len(permutation)):
+                if permutation[j] > current_subsequence[-1]:
+                    current_subsequence.append(permutation[j])
+                    longest_subsequence(j, permutation)
+            subsequences.append(current_subsequence)
+
+
+print(longest_subsequence(0, [8, 2, 1, 6, 5, 7, 4, 3, 9]))
