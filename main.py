5d88097c7899d2ef90e4a9e9e2e7c72268ef86da
James Bedford <jamesbedford@protonmail.com>
2021-04-30 19:49:35 +1000

    Working recursion

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-def longest_subsequence(start_position, permutation):
-    subsequences = []
-    if start_position == len(permutation):
-        return subsequences
+def longest_subsequence(permutation, subsequences):
+    if len(permutation) == 1:
+        return permutation
     else:
-        for i in permutation:
-            current_subsequence = [i]
-            for j in range(start_position, len(permutation)):
+        for i in range(len(permutation)):
+            current_subsequence = [permutation[i]]
+            for j in range(i+1, len(permutation)):
                 if permutation[j] > current_subsequence[-1]:
                     current_subsequence.append(permutation[j])
-                    longest_subsequence(i, permutation)
+                    longest_subsequence(permutation[j:], subsequences)
             subsequences.append(current_subsequence)
+        return subsequences
 
 
-print(longest_subsequence(0, [8, 2, 1, 6, 5, 7, 4, 3, 9]))
+print(longest_subsequence([8, 2, 1, 6, 5, 7, 4, 3, 9], []))
