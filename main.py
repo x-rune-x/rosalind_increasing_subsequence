32c3f4cdfecf78a0ab2317845d851b4d1d260890
James Bedford <jamesbedford@protonmail.com>
2021-05-05 11:15:39 +1000

    Modified solution but still not correct

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-def longest_subsequence(permutation, subsequences):
+def increasing_subsequence(permutation, subsequences):
     if len(permutation) == 1:
         return permutation
     else:
         for i in range(len(permutation)):
-            current_subsequence = [permutation[i]]
+            larger_numbers = []
             for j in range(i+1, len(permutation)):
-                if permutation[j] > current_subsequence[-1]:
-                    current_subsequence.append(permutation[j])
-                    longest_subsequence(permutation[j:], subsequences)
-            subsequences.append(current_subsequence)
-        return subsequences
+                if j > i:
+                    larger_numbers.append(j)
+                increasing_subsequence(larger_numbers, subsequences)
+            subsequences.append(larger_numbers)
+    return max(subsequences, key=len)
 
 
-print(longest_subsequence([8, 2, 1, 6, 5, 7, 4, 3, 9], []))
+print(increasing_subsequence([9, 1, 6, 2, 5, 7, 4, 8, 3], []))
