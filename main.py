6f67d828d5b45624b456531e9e17bdcb2912b967
James Bedford <jamesbedford@protonmail.com>
2021-05-11 21:39:59 +1000

    Solution works for small permutations but takes too long for large inputs

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
 def increasing_subsequence(permutation):
     subsequences = []
+    for x in range(len(permutation)):
+        subsequences.append([[permutation[x]]])
     for i in range(len(permutation)):
-        for j in range(len(permutation)-1, i, -1):
-            subsequences.append([permutation[i]] + permutation[j:])
-            for k in range(j-1, i, -1):
-                subsequences.append([permutation[i]] + permutation[k:j])
-
-    for x in range(len(subsequences)):
-        print(x)
-        sub = subsequences[x]
-        print(sub)
-        for pos in range(len(sub)-1):
-            if sub[pos] > sub[pos+1]:
-                subsequences[x] = ''
-
-    return subsequences
+        for j in range(i):
+            for sub in subsequences[j]:
+                if permutation[i] > sub[-1]:
+                    subsequences[j].append(sub + [permutation[i]])
+    increasing_subs = []
+    for long_sub in subsequences:
+        increasing_subs.append(max(long_sub, key=len))
+    return max(increasing_subs, key=len)
 
 
-f = increasing_subsequence([9, 1, 6, 2, 5, 7, 4, 8, 3])
-print(f)
+def decreasing_subsequence(permutation):
+    subsequences = []
+    for x in range(len(permutation)):
+        subsequences.append([[permutation[x]]])
+    for i in range(len(permutation)):
+        for j in range(i):
+            for sub in subsequences[j]:
+                if permutation[i] < sub[-1]:
+                    subsequences[j].append(sub + [permutation[i]])
+    decreasing_subs = []
+    for long_sub in subsequences:
+        decreasing_subs.append(max(long_sub, key=len))
+    return max(decreasing_subs, key=len)
+
+
+def process_file(file_name):
+    file = open(file_name, 'r')
+    file.readline()
+    permutation = file.read()
+    file.close()
+
+    permutation_list = permutation.split()
+
+    print(permutation_list)
+
+    increasing_sub = increasing_subsequence(permutation_list)
+    decreasing_sub = decreasing_subsequence(permutation_list)
+
+    string_increasing = ' '.join(increasing_sub)
+    string_decreasing = ' '.join(decreasing_sub)
+
+    output_file = open('solution.txt', 'w')
+    output_file.write(string_increasing + '\n')
+    output_file.write(string_decreasing)
+    output_file.close()
+
+
+process_file('rosalind_lgis.txt')
